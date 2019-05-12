6827423aac2f9e2c217c62a2849551e48bab1bbc
Olaitan Majek <majekolaitan@gmail.com>
2019-05-12 19:39:20 +0400

    Code reset - no changes

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -4,26 +4,26 @@ rename = require('gulp-rename'),
 del = require('del');
 
 var config = {
-	mode: {
-		css: {
-			sprite: 'sprite.svg',
-			render: {
-				css: {
-					template: './gulp/templates/sprite.css'
-				}
-			}
-		}
-	}
+  mode: {
+    css: {
+      sprite: 'sprite.svg',
+      render: {
+        css: {
+          template: './gulp/templates/sprite.css'
+        }
+      }
+    }
+  }
 }
 
 gulp.task('beginClean', function() {
-	return del(['./app/temp/sprite', './app/assets/images/sprites']);
-})
+  return del(['./app/temp/sprite', './app/assets/images/sprites']);
+});
 
-gulp.task('createSprite', ['beginClean'] , function() {
-	return gulp.src('./app/assets/images/icons/**/*.svg')
-		.pipe(svgSprite(config))
-		.pipe(gulp.dest('./app/temp/sprite'));
+gulp.task('createSprite', ['beginClean'], function() {
+  return gulp.src('./app/assets/images/icons/**/*.svg')
+    .pipe(svgSprite(config))
+    .pipe(gulp.dest('./app/temp/sprite/'));
 });
 
 gulp.task('copySpriteGraphic', ['createSprite'], function() {
@@ -31,14 +31,14 @@ gulp.task('copySpriteGraphic', ['createSprite'], function() {
     .pipe(gulp.dest('./app/assets/images/sprites'));
 });
 
-gulp.task('copySpriteCSS', ['createSprite'] , function() {
-	return gulp.src('./app/temp/sprite/css/*.css')
-		.pipe(rename('_sprite.css'))
-		.pipe(gulp.dest('./app/assets/styles/modules'));
+gulp.task('copySpriteCSS', ['createSprite'], function() {
+  return gulp.src('./app/temp/sprite/css/*.css')
+    .pipe(rename('_sprite.css'))
+    .pipe(gulp.dest('./app/assets/styles/modules'));
 });
 
-gulp.task('endClean', ['copySpriteGraphic', 'copySpriteCSS'], function () {
-	return del('./app/temp/sprite');
+gulp.task('endClean', ['copySpriteGraphic', 'copySpriteCSS'], function() {
+  return del('./app/temp/sprite');
 });
 
-gulp.task('icons', ['beginClean', 'createSprite', 'copySpriteGraphic' , 'copySpriteCSS', 'endClean']);
+gulp.task('icons', ['beginClean', 'createSprite', 'copySpriteGraphic', 'copySpriteCSS', 'endClean']);
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -4,24 +4,24 @@ browserSync = require('browser-sync').create();
 
 gulp.task('watch', function() {
 
-	browserSync.init({
-		notify: false,
-		server: {
-			baseDir: "app"
-		}
-	});
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: "app"
+    }
+  });
 
-	watch('./app/index.html', function() {
-		browserSync.reload();
-	});
+  watch('./app/index.html', function() {
+    browserSync.reload();
+  });
 
-	watch('./app/assets/styles/**/*.css', function() {
-		gulp.start('cssInject');
-	});
+  watch('./app/assets/styles/**/*.css', function() {
+    gulp.start('cssInject');
+  });
 
 });
 
-gulp.task('cssInject', ['styles'] , function() {
-	return gulp.src('./app/temp/styles/styles.css')
-		.pipe(browserSync.stream());
+gulp.task('cssInject', ['styles'], function() {
+  return gulp.src('./app/temp/styles/styles.css')
+    .pipe(browserSync.stream());
 });
